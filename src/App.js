d3b442785d628de7b9e4f339a1f800d4525ad8bf
Taro Yoko <taroyoko@Taros-MacBook-Pro.local>
2022-04-27 05:18:52 -0400

    commentary

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import ToDoList from './components/TodoList';
+
+function App() {
+  return (
+    <div className='todo-app'>
+      <ToDoList />
+
+    </div>
+  );
+}
+
+export default App;
